07e48b5f64b50aec69c807505fb9740d7af05220
ITSHDRpi4 <help@sandiego.edu>
2025-03-25 12:05:47 -0700

    Renamed user to user_id for ActiveClaim, added username and fullname fields

--- activeclaim/models.py
+++ activeclaim/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 class ActiveClaim(models.Model):
     casenum = models.CharField(max_length=8, unique=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     claim_time = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
-        return f"ID: {self.pk}, Casenum: {self.casenum}, User: {self.user.email}, Claim Time: {self.claim_time}"
+        return f"ID: {self.pk}, Casenum: {self.casenum}, User ID: {self.user.email}, Claim Time: {self.claim_time}"
--- activeclaim/serializers.py
+++ activeclaim/serializers.py
@@ -3,6 +3,17 @@ from activeclaim.models import ActiveClaim
 
 
 class ActiveClaimSerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = ActiveClaim
         fields = '__all__'
+    
+    def get_user_name(self, obj):
+        return obj.user_id.username if obj.user_id else None
+    
+    def get_full_name(self, obj):
+        if obj.user_id:
+            return f"{obj.user_id.first_name} {obj.user_id.last_name}".strip()
+        return None
