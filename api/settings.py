5dab1a63fb10d80b3b414a894af3b4baf2c51ae9
ITSHDRpi4 <help@sandiego.edu>
2025-03-25 11:25:56 -0700

    Moved all important information to env file

--- api/settings.py
+++ api/settings.py
@@ -9,6 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 from pathlib import Path
 
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ikio96a1#8rlfm1)n6(lqod55#6zu8lufhtd!_w5bi9(t0_zoc'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG'] == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = json.loads(os.environ["ALLOWED_HOSTS"])
 
 
 # Application definition
