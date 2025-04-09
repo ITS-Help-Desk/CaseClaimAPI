d979708983b69256cb9ae8e99c7ed091a71c70db
zletcher <zletcher@sandiego.edu>
2025-04-09 15:23:19 -0700

    Renamed Admin Page

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('caseflow-admin/', admin.site.urls),
     path('api/ping/', include('ping.urls')),
     path('api/user/', include('user.urls')),
     path('api/activeclaim/', include('activeclaim.urls')),
