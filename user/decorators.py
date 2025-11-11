0deee4a99d287b815f9925ce484bc3720cc93845
zletcher <zletcher@sandiego.edu>
2025-11-11 09:37:49 -0800

    Setting up user roles(tracked as: groups)

--- user/decorators.py
+++ user/decorators.py
@@ -2,11 +2,16 @@ from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import user_passes_test
 
 
-def group_required(group_name):
-    """Decorator to check if a user is in a specific group and return 403 if not."""
+def group_required(group_names):
+    """Decorator to check if a user is in any of the specified groups and return 403 if not."""
     def decorator(view_func):
         def _wrapped_view(request, *args, **kwargs):
-            if not request.user.groups.filter(name=group_name).exists():
+            if isinstance(group_names, str):
+                groups = [group_names]
+            else:
+                groups = group_names
+
+            if not any(request.user.groups.filter(name=group).exists() for group in groups):
                 return HttpResponseForbidden("You do not have permission to access this page.")
             return view_func(request, *args, **kwargs)
         return _wrapped_view
--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework.serializers import ModelSerializer
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class GroupSerializer(ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'name']
 
 class UserSerializer(ModelSerializer):
+    groups = GroupSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'groups']
